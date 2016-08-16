cb04f2a312aa8e5ac00733c7991cf44993b5c7d6
Junghee Roh <junghee.roh@windriver.com>
2016-08-16 14:25:36 +0900

    Insert more sentence.

--- sentence_main.py
+++ sentence_main.py
@@ -4,7 +4,8 @@
 import sys, os, random
 
 DATA_FILE = "sentence.data"
-RETRY_ANSWER_MAX = 3
+RETRY_ANSWER_MAX = 2
+REVIEW_COUNT = 3
 data_dict = {}
 
 def init():
@@ -12,6 +13,12 @@ def init():
     for line in fi.readlines():
         array = line.split('/')
         data_dict[array[1].strip()] = array[0].strip()
+
+def review_failed(item):
+    for x in xrange(REVIEW_COUNT):
+        val = raw_input("%s : "%item)
+        if (data_dict[item] != val):
+            print "정답 : %s"% data_dict[item]
 
 def practice_do(problme):
     cnt = 0
@@ -23,18 +30,29 @@ def practice_do(problme):
     print "정답 : %s"% data_dict[problme]
     return 0
 
-def practice_start():
-    keys = data_dict.keys()
+def practice_start(practice_list):
+    total_cnt = len(practice_list)
+
+    if (total_cnt == 0):
+        return
+
     right_cnt = 0
-    while(len(keys) != 0):
-        index = random.randrange(0, len(keys))
-        right_cnt += practice_do(keys[index])
-        keys.remove(keys[index])
-    print "총 %d개 맞춘 수 %d개 점수 %d점"%(len(data_dict), right_cnt, right_cnt*100/len(data_dict))
+    failed_list = []
+    while(len(practice_list) != 0):
+        index = random.randrange(0, len(practice_list))
+        item = practice_list[index]
+        if (practice_do(item) == 0):
+            failed_list.append(item)
+            review_failed(item)
+        else:
+            right_cnt += 1
+        practice_list.remove(item)
+    print "총 %d개 맞춘 수 %d개 점수 %d점"%(total_cnt, right_cnt, right_cnt*100/total_cnt)
+    practice_start(failed_list)
 
 def main():
     init()
-    practice_start()
+    practice_start(data_dict.keys())
 
 if __name__ == '__main__':
     main()
